f0b43919e158f2da3ee819ef0c1dd6e61ae7e4ac
SID-RA <sid89sidra@gmail.com>
2021-02-13 22:55:42 +0500

    complete app

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="text-center text-white mb-3">
+      <h2>TodoApp</h2>
+      <h5>By Sidra Saleem </h5>
+      <h5>Roll No. BCC037965</h5>
+    </div>
+  );
+}
